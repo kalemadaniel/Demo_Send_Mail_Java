6d74f8e62968996168da82756374aa7eddfa7cac
kalemadaniel <kalemadaniel2014@gmail.com>
2021-09-21 13:11:59 +0200

    frame principal 11

--- src/demo_send_mail_java/Principal.java
+++ src/demo_send_mail_java/Principal.java
@@ -65,6 +65,11 @@ public class Principal extends javax.swing.JFrame {
         jScrollPane1.setViewportView(txtMessage);
 
         jButton1.setText("Send");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -131,6 +136,14 @@ public class Principal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        String ToEmail=txtToMail.getText();
+        String FromEmail =txtFromEmail.getText();
+        String Subject=txtSubject.getText();
+        String Message=txtMessage.getText();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
